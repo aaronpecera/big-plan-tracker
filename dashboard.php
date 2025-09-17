d485eaa3acca714d8fed3954556b914bf03cd312
Aaron Gorman <aaron.gorman@grupopecera.com>
2025-09-17 13:56:42 +0200

    fix dashboard

--- dashboard.php
+++ dashboard.php
@@ -9,7 +9,7 @@ session_start();
 // Check if user is logged in
 if (!isset($_SESSION['user_id'])) {
     // Redirect to login page
-    header('Location: /views/login.php');
+    header('Location: /login');
     exit;
 }
 
